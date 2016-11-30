dfeaca6b6e5dfc2ffb8cb4441b13c3db6f29323a
Yaron Cohen <yaroncnk@gmail.com>
2016-11-30 04:02:19 +0000

    created an array of objects for eventbrite tech events in Montreal

--- eventbrite.js
+++ eventbrite.js
@@ -16,21 +16,42 @@ function requestPromise(url) {
 
 //getting events from Eventbrite parsed as JSON
 function getEvents() {
+    var website = 'https://www.eventbriteapi.com/v3/events/search/?token=';
+    var filter = '&categories=102&location.latitude=45.5087&location.longitude=-73.554&location.within=20km';
+    var token = 'TH7YBHKCSVXFCHI74NOV';
     return (
-        requestPromise('https://www.eventbrite.com/d/canada--montreal/science-and-tech--events/oauth/authorize?response_type=code&client_id=138316538378882428467')
+        requestPromise(website+token+filter)
         .then(function(events) {
-            var latestEvent = JSON.parse(events.body);
-            return latestEvent; 
-
+        
+            var event = JSON.parse(events.body);
+            var listOfEvents = event.events;
+            var arrayOfEvents = [];
+        
+            // returning a beautiful array of objects with events
+            listOfEvents.forEach(function(techEvent){
+                
+                arrayOfEvents.push( {
+                    name: techEvent.name.text,
+                    description: techEvent.description.text,
+                    url: techEvent.url,
+                    startTime: techEvent.start.local,
+                    endTime: techEvent.end.local
+                } );
+                
+            });
+            return arrayOfEvents;
+            
         })
+    
     );
 }
 
-getEvents().then(function(events){
-    console.log(events) ;   
+//just testing what I have done
+getEvents().then(function(event){
+
+    console.log(event)
+        
     
 })
 
-.catch(function(err){
-    console.log(err);
-});
+
